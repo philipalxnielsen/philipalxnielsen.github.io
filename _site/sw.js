dd1aaf819beccb5a6f7f8d2a87ce2cf44e12c6bf
Philip <78845767+dragonballphilip@users.noreply.github.com>
2022-12-05 21:37:55 +0100

    About page

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221205.212647';
+const cacheName = 'chirpy-20221205.213734';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
