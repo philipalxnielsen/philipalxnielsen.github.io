0e63a7dff11f0997aff5756e26c4e384c461ebb8
Philip <78845767+dragonballphilip@users.noreply.github.com>
2022-12-06 13:14:54 +0100

    Resusme

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221205.214446';
+const cacheName = 'chirpy-20221206.131436';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
